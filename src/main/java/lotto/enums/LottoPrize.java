f0154f475d1816bfec5af1ba963bd12ce1d1cd18
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-11-03 15:21:52 +0900

    feat(LottoPrize.java): add rank, message and methods in Lotto Prize enum
    Lotto Prize enum 등수, 출력메시지 추가
    
    메소드 추가:
    - getPrizeByRank(rank) : 등수에 맞는 상금 리턴
    - getPrizeMessageByRank(rank) : 등수에 맞는 메시지 리턴
    - getter 추가

--- src/main/java/lotto/enums/LottoPrize.java
+++ src/main/java/lotto/enums/LottoPrize.java
@@ -4,21 +4,25 @@ import java.util.Arrays;
 
 public enum LottoPrize {
 
-    RANK_1(2_000_000_000, 6, false),
-    RANK_2(30_000_000, 5, true),
-    RANK_3(1_500_000, 5, false),
-    RANK_4(50_000, 4, false),
-    RANK_5(5_000, 3, false),
-    NONE(0, 0, false);
+    RANK_1(1,2_000_000_000, 6, false,"6개 일치 (2,000,000,000원)"),
+    RANK_2(2,30_000_000, 5, true, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    RANK_3(3,1_500_000, 5, false, "5개 일치 (1,500,000원)"),
+    RANK_4(4,50_000, 4, false,"4개 일치 (50,000원)"),
+    RANK_5(5,5_000, 3, false,"3개 일치 (5,000원)"),
+    NONE(0,0, 0, false,"");
 
+    private final int rank;
     private final int prize;
     private final int matchCount;
     private final boolean needsBonus; // 보너스 볼이 필요한지 여부
+    private final String prizeMessage;
 
-    LottoPrize(int prize, int matchCount, boolean needsBonus) {
+    LottoPrize(int rank, int prize, int matchCount, boolean needsBonus, String prizeMessage) {
+        this.rank = rank;
         this.prize = prize;
         this.matchCount = matchCount;
         this.needsBonus = needsBonus;
+        this.prizeMessage = prizeMessage;
     }
 
     /**
@@ -41,11 +45,35 @@ public enum LottoPrize {
                 .orElse(NONE);
     }
 
+    public static long getPrizeByRank(int rank) {
+        return Arrays.stream(values())
+                .filter(prize -> prize.rank == rank)
+                .findFirst()
+                .map(LottoPrize::getPrize)
+                .orElse(0);
+    }
+
+    public static String getPrizeMessageByRank(int rank){
+        return Arrays.stream(values())
+                .filter(prize -> prize.rank == rank)
+                .findFirst()
+                .map(LottoPrize::getPrizeMessage)
+                .orElse("");
+    }
+
     public int getPrize() {
         return prize;
     }
 
+    public int getRank(){
+        return rank;
+    }
+
     public int getMatchCount() {
         return matchCount;
+    }
+
+    public String getPrizeMessage(){
+        return prizeMessage;
     }
 }
