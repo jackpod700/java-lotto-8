6c6babb8610089c017d66c05efdcfa4d3658c1b6
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-11-03 11:20:27 +0900

    feat(InputParser.java): implement Input Parser

---
+++ src/main/java/lotto/util/InputParser.java
@@ -0,0 +1,27 @@
+package lotto.util;
+
+import static lotto.enums.LottoConstant.LOTTO_PRICE;
+import static lotto.util.InputValidator.NUMBER_DELIMITER;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputParser {
+
+    public static int parsePurchaseAmount(String input){
+        return Integer.parseInt(input)/ LOTTO_PRICE.getValue();
+    }
+
+    public static List<Integer> parseWinningNumbers(String input){
+        List<Integer> winningNumbers;
+        winningNumbers = Arrays.stream(input.split(NUMBER_DELIMITER))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+        return winningNumbers;
+    }
+
+    public static int parseBonusNumber(String input){
+        return Integer.parseInt(input);
+    }
+}
