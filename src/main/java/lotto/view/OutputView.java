7bac8ae10e6c2e3cf119bcffe5fe5f3d7bf59e1a
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-11-03 17:19:45 +0900

    refactor(OutputView.java): change strings to variable
    하드코딩 문자열들 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,22 +7,26 @@ import java.util.Map;
 import lotto.model.Lotto;
 
 public class OutputView {
-    public static void printLottos(int ticketCount, List<Lotto> lottos){
-        System.out.println("\n"+ticketCount + "개를 구매했습니다.");
-        for(Lotto lotto : lottos){
+    private static final String PURCHASE_COUNT_MESSAGE = "\n%d개를 구매했습니다.\n";
+    private static final String PRE_RESULT_MESSAGE = "\n당첨 통계\n---\n";
+    private static final String RANK_RESULT_MESSAGE = " - %d개\n";
+    private static final String PROFIT_RESULT_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
+    public static void printLottos(int ticketCount, List<Lotto> lottos) {
+        System.out.printf((PURCHASE_COUNT_MESSAGE), ticketCount);
+        for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbersAsString());
         }
     }
 
-    public static void printResult(Map<Integer, Long> prizeCounts, Double profitRate){
-        System.out.println("\n당첨 통계");
-        System.out.println("---");
+    public static void printResult(Map<Integer, Long> prizeCounts, Double profitRate) {
+        System.out.printf(PRE_RESULT_MESSAGE);
 
-        for(int rank=5;rank>0;rank--){
-            System.out.printf(getPrizeMessageByRank(rank)
-                    +" - %d개%n",prizeCounts.getOrDefault(rank, 0L));
+        for (int rank = 5; rank > 0; rank--) {
+            System.out.printf(getPrizeMessageByRank(rank) + RANK_RESULT_MESSAGE
+                    , prizeCounts.getOrDefault(rank, 0L));
         }
 
-        System.out.printf("총 수익률은 %.1f%%입니다.", profitRate);
+        System.out.printf(PROFIT_RESULT_MESSAGE, profitRate);
     }
 }
